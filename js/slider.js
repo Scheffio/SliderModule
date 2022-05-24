27b44cfd88ac5739cc07f1ab12844b40fb8e0d2d
Scheffio <assi1122334455@gmail.com>
2022-05-24 19:23:08 +0200

    v1.1

--- js/slider.js
+++ js/slider.js
@@ -3,8 +3,9 @@
 //в дальнейшем, если в html создать блок с этим классом, то модуль
 //будет создаваться на его месте
 const blockToConnect = document.querySelector('.slider-module')
-
 const slider = {
+    position: 0,
+    max: 380 * 11,
     //Для собственного удобства, создаю функцию, которая будет
     //генерировать нужный мне элемент с нужными свойствами
     generateElement(element, elementClass, elementContent) {
@@ -36,21 +37,36 @@ const slider = {
         let slider = blockToConnect.appendChild(this.generateElement('div','slider','')).appendChild(this.generateElement('div','slider-container','')).appendChild(this.generateElement('div','slider-track',''))
         
         // Потом элементы, пока два, для теста, потом будет 12 как и просили
-        for(let i = 0; i < 2; i ++) {slider.appendChild(this.generateElement('div','slider-item',''))}
+        for(let i = 0; i < 12; i ++) {slider.appendChild(this.generateElement('div','slider-item',''))}
         
         // Здесь уже карточки делаем
-        document.querySelectorAll('.slider-item').forEach((element, index) => element.appendChild(this.generateElement('div','item-about',`<p>${index + 1}</p>`)))
+        document.querySelectorAll('.slider-item').forEach((element, index) => element.appendChild(this.generateElement('div','item-about',`<p>${index% 9 + 1}</p>`)))
         document.querySelectorAll('.item-about').forEach((element, index) => element.appendChild(this.generateElement('div','item-name',`<p>таунхаус</p><p>ул. свободы</p><p>дом № ${index + 1}А</p><p>5 100 00 руб.</p>`)))
         
         document.querySelectorAll('.slider-item').forEach((element) => element.appendChild(this.generateElement('img', '', '')).setAttribute('src', '/imgs/img.png'))
         document.querySelectorAll('.slider-item').forEach((element) => element.appendChild(this.generateElement('a', 'link', '<img src="/imgs/linkArrow.svg" alt="arrow">')).setAttribute('href', '#'))
 
-        /* Это кнопки слайдера, они ещё не доделаны. 5 утра всё таки, блин
         document.querySelector('.slider').appendChild(this.generateElement('div','slider-btns','')).appendChild(this.generateElement('div','btn-prev',''))
         document.querySelector('.slider-btns').appendChild(this.generateElement('div', 'btn-next',''))
-        */
-        
+    },
+    nextBtn() {
+        this.position += 380
+        if(this.position >= this.max) {
+            this.position = 0
+        }
+        document.querySelector('.slider-track').style.right = `${this.position}px`
+    },
+    prevBtn() {
+        this.position -= 380
+        if(this.position < 0) {
+            this.position = this.max - 380
+        }
+        document.querySelector('.slider-track').style.right = `${this.position}px`
     }
 }
 
 slider.render()
+
+
+document.querySelector('.btn-next').addEventListener('click', () => slider.nextBtn())
+document.querySelector('.btn-prev').addEventListener('click', () => slider.prevBtn())
